a180fbb7bf7908f798fa4d16e09b49c601d4d63e
UttU28 <utsavmaan28@gmail.com>
2025-01-17 17:27:15 -0600

    this wokring

--- dataScraping.py
+++ dataScraping.py
@@ -9,7 +9,17 @@ from addToJSON import *
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
+from dotenv import load_dotenv
+import os
 
+# Load environment variables
+load_dotenv()
+
+# Environment Variables
+CHROME_DRIVER_PATH = os.getenv('CHROME_DRIVER_PATH')
+CHROME_APP_PATH = os.getenv('CHROME_APP_PATH')
+CHROME_USER_DATA_DIR = os.getenv('CHROME_USER_DATA_DIR')
+DEBUGGING_PORT = os.getenv('DEBUGGING_PORT')
 
 def pageLoadHoneDe(driver1):
     WebDriverWait(driver1, 10).until(
@@ -19,21 +29,21 @@ def pageLoadHoneDe(driver1):
 def readingBhawishyawaniPage(driver1):
     notMyCompany = ["Dice", "Epic", "Jobot", "ClickJobs.io"]
 
-
-    currentPageData = driver1.find_element(By.CLASS_NAME, "scaffold-layout__list-container")
+    currentPageData = driver1.find_element(By.CLASS_NAME, "scaffold-layout__list")
     jobPostings = currentPageData.find_elements(By.CLASS_NAME, "job-card-container--clickable")
     print(len(jobPostings))
 
     for posting in jobPostings:
         try:
-            # posting.click()
             id = posting.get_attribute("data-job-id")
             data = posting.find_element(By.CLASS_NAME, "job-card-container__link")
             title = data.text.strip()
             link = data.get_attribute('href')
-            companyName = posting.find_element(By.CLASS_NAME, "job-card-container__primary-description ").text.strip()
-            location = posting.find_element(By.CLASS_NAME, "job-card-container__metadata-item ").text.strip()
+            companyName = posting.find_element(By.CLASS_NAME, "artdeco-entity-lockup__subtitle ").text.strip()
+            location = posting.find_element(By.CLASS_NAME, "artdeco-entity-lockup__caption ").text.strip()
             applyMethod = None
+
+            print(id, title, companyName, location)
 
             if checkBhawishyaWani(id) and companyName not in notMyCompany:
                 try:
@@ -45,33 +55,47 @@ def readingBhawishyawaniPage(driver1):
                 posting.click()
                 sleep(1)
                 jobType = driver.find_element(By.CSS_SELECTOR, 'li.job-details-jobs-unified-top-card__job-insight').text
-                jobDescription = driver.find_element(By.CLASS_NAME, "jobs-description-content__text").text
-                addBhawishyaWani(id, link, title, companyName, location, applyMethod, time.time(), jobType, jobDescription)
+                jobDescription = driver.find_element(By.CLASS_NAME, "jobs-description__container").text
+                print(jobDescription)
+                addBhawishyaWani(id, link, title, companyName, location, applyMethod, time.time(), 'FullTime', jobDescription)
 
         except Exception as e:
             print(f"Error in readingBhawishyawaniPage: {e}")
 
 if __name__ == "__main__":
-    chrome_driver_path = 'C:/chromeDriver/chromedriver.exe'  # Ensure the path is correct
-    chromeApp = subprocess.Popen(['C:/Program Files/Google/Chrome/Application/chrome.exe', '--remote-debugging-port=9003', '--user-data-dir=C:/chromeDriver/letsCheck/'])
+    chrome_data_dir = os.path.join(os.getcwd(), 'chromeData')
+    if not os.path.exists(chrome_data_dir):
+        os.makedirs(chrome_data_dir)
+        print(f"'{chrome_data_dir}' directory was created.")
+    else:
+        print(f"'{chrome_data_dir}' directory already exists.")
+
+    chromeApp = subprocess.Popen([
+        CHROME_APP_PATH,
+        f'--remote-debugging-port={DEBUGGING_PORT}',
+        f'--user-data-dir={CHROME_USER_DATA_DIR}'
+    ])
     sleep(2)
+
+    # Configure WebDriver
     options = Options()
-    options.add_experimental_option("debuggerAddress", "localhost:9003")
-    options.add_argument(f"webdriver.chrome.driver={chrome_driver_path}")
+    options.add_experimental_option("debuggerAddress", f"localhost:{DEBUGGING_PORT}")
+    options.add_argument(f"webdriver.chrome.driver={CHROME_DRIVER_PATH}")
     options.add_argument("--disable-notifications")
     driver = webdriver.Chrome(options=options)
 
+    # Job Keywords List
     myList = ["devops*", "cloud engineer", "cloud architect", "site reliability", "platform engineer", "aws", "azure"]
     for eachElement in myList:
         currentPage = 0
-        searchText = eachElement.strip().replace(" ","%20")
+        searchText = eachElement.strip().replace(" ", "%20")
         print(searchText)
         while True:
             pageURL = f"https://www.linkedin.com/jobs/search/?&distance=25.0&f_JT=F%2CC&f_T=25764%2C30006%2C6483%2C22848%2C25165&f_TPR=r86400&geoId=103644278&keywords={searchText}&origin=JOB_SEARCH_PAGE_SEARCH_BUTTON&refresh=true&sortBy=DD&spellCorrectionEnabled=true&start="
-            driver.get(pageURL+str(currentPage*25))
+            driver.get(pageURL + str(currentPage * 25))
             currentPage += 1
             sleep(5)
-            try: 
+            try:
                 driver.find_element(By.CLASS_NAME, "jobs-search-no-results-banner")
                 print("All Jobs have been scraped")
                 break
